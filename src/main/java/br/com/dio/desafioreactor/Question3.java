2a84c923390ca9170e5ace3b40582c1ffdbaf9e9
renatobarbosazup <renato.barbosa@zup.com.br>
2023-07-25 19:19:58 -0300

    Resolução do desafio

--- src/main/java/br/com/dio/desafioreactor/Question3.java
+++ src/main/java/br/com/dio/desafioreactor/Question3.java
@@ -2,12 +2,19 @@ package br.com.dio.desafioreactor;
 
 import reactor.core.publisher.Mono;
 
+import java.util.Optional;
+
 public class Question3 {
 
     /*
     Verifica se o usuário passado é valido, caso seja retorna void, caso contrário deve disparar uma exception
     (para esse desafio vamos considerar que o usário é valido quando ele tem uma senha com mais de 8 caractéres)
      */
-    public Mono<Void> userIsValid(final User user){}
+    public Mono<Void> userIsValid(final User user){
+        return Mono.justOrEmpty(Optional.ofNullable(user))
+                .filter(it -> it.password() != null && it.password().length() > 8)
+                .switchIfEmpty(Mono.defer(() -> Mono.error(new RuntimeException("Usuário inválido"))))
+                .then();
+    }
 
 }
